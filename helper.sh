cee1aba53772378f6a45063c61d6e04df225967a
Josa Gesell <josa@gesell.me>
2018-01-30 15:42:53 +0100

    Add split

--- helper.sh
+++ helper.sh
@@ -17,3 +17,20 @@ contains() {
   for e; do [[ "$e" == "$match" ]] && return 0; done
   return 1
 }
+
+# Example
+# 
+# ```
+# array=($(split "cat,dog" ","))
+# for element in ${array[@]}; do
+#   echo "- $element"
+# done
+# ```
+#
+# $1 : string
+# $2 : separator
+split() {
+  IFS="$2" read -ra array <<< "$1"
+  echo "${array[@]}";
+}
+
